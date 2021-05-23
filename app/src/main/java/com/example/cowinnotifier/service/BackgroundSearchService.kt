e41a46b2251285c1d03fe9b9ed855e71d527eec7
Ujjwal <ujjwalkr.maharana@gmail.com>
2021-05-23 17:00:17 +0530

    changed to job scheduler. Updated result screen UI

--- app/src/main/java/com/example/cowinnotifier/service/BackgroundSearchService.kt
+++ app/src/main/java/com/example/cowinnotifier/service/BackgroundSearchService.kt
@@ -1,5 +1,7 @@
 package com.example.cowinnotifier.service
 
+import android.app.job.JobParameters
+import android.app.job.JobService
 import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
@@ -8,21 +10,23 @@ import com.example.cowinnotifier.MyApplication
 import com.example.cowinnotifier.helper.AppConstants
 import com.example.cowinnotifier.model.Center
 import com.example.cowinnotifier.repository.retrofit.APIService
+import com.example.cowinnotifier.utils.CoroutineUtil
 import com.example.cowinnotifier.utils.NotificationUtil
 import com.example.cowinnotifier.utils.SessionUtil
+import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
 
-class BackgroundSearchService(context: Context, workerParams: WorkerParameters) :
-    CoroutineWorker(context, workerParams) {
+@AndroidEntryPoint
+class BackgroundSearchService() : JobService() {
 
     @Inject
     lateinit var apiService: APIService
     val TAG = "customtag"
 
-    override suspend fun doWork(): Result {
-        Log.d(TAG, "Inside doWork: ")
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d(TAG, "Inside onStartJob: ")
 
         val pincode = MyApplication.sharedPreferences.getString(AppConstants.PINCODE, "-1")
         val districtId = MyApplication.sharedPreferences.getString(AppConstants.DISTRICT_ID, "-1")
@@ -31,38 +35,50 @@ class BackgroundSearchService(context: Context, workerParams: WorkerParameters) 
 
         if (pincode != "-1") {
             Log.d(TAG, "Pincode search")
-            searchForAvailableSlots(pincode!!, AppConstants.PINCODE)
+            searchForAvailableSlots(pincode!!, AppConstants.PINCODE, params)
         } else if (districtId != "-1") {
             Log.d(TAG, "district search")
-            searchForAvailableSlots(districtId!!, AppConstants.DISTRICT_ID)
+            searchForAvailableSlots(districtId!!, AppConstants.DISTRICT_ID, params)
         }
-        Log.d(TAG, "returning success from dowWork")
-        return Result.success()
+        Log.d(TAG, "returning true from startJob")
+
+        return true
     }
 
-    private suspend fun searchForAvailableSlots(queryParam: String, queryName: String) {
-        Log.d(TAG, "searchForAvailableSlots: ")
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return false
+    }
 
-        val currentDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+    private fun searchForAvailableSlots(
+        queryParam: String,
+        queryName: String,
+        params: JobParameters?
+    ) {
+        CoroutineUtil.io {
+            Log.d(TAG, "searchForAvailableSlots: ")
 
-        val centerList: List<Center> =
-            if (queryName == AppConstants.PINCODE)
-                apiService.getCalendarByPin(queryParam, currentDate).centerList
-            else
-                apiService.getCalendarByDistrict(queryParam, currentDate).centerList
+            val currentDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
 
-        Log.d(TAG, "centerList: $centerList")
+            val centerList: List<Center> =
+                if (queryName == AppConstants.PINCODE)
+                    apiService.getCalendarByPin(queryParam, currentDate).centerList
+                else
+                    apiService.getCalendarByDistrict(queryParam, currentDate).centerList
 
-        for (eachCenter in centerList) {
-            val sessionList = eachCenter.sessions
-            Log.d(TAG, "SessionList: $sessionList")
+            Log.d(TAG, "centerList: $centerList")
 
-            for (eachSession in sessionList) {
-                if (SessionUtil.isValidSession(eachSession)) {
-                    Log.d(TAG, "Inside is valid session")
-                    NotificationUtil.showNotification(applicationContext, eachCenter, eachSession)
+            for (eachCenter in centerList) {
+                val sessionList = eachCenter.sessions
+                Log.d(TAG, "SessionList: $sessionList")
+
+                for (eachSession in sessionList) {
+                    if (SessionUtil.isValidSession(eachSession)) {
+                        Log.d(TAG, "Inside is valid session")
+                        NotificationUtil.showNotification(applicationContext, eachCenter, eachSession)
+                    }
                 }
             }
+            jobFinished(params, false)
         }
     }
 }
--- app/src/main/java/com/example/cowinnotifier/utils/SchedulerUtil.kt
+++ app/src/main/java/com/example/cowinnotifier/utils/SchedulerUtil.kt
@@ -1,34 +1,48 @@
 package com.example.cowinnotifier.utils
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.content.Context
-import androidx.work.*
 import com.example.cowinnotifier.helper.AppConstants
 import com.example.cowinnotifier.service.BackgroundSearchService
-import java.util.concurrent.TimeUnit
 
 class SchedulerUtil {
 
     companion object {
+
         fun scheduleNewWork(context: Context) {
+            val jobScheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            val jobInfo = JobInfo.Builder(100, ComponentName(context, BackgroundSearchService::class.java))
 
-            val constraints =
-                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-
-            val workRequest = PeriodicWorkRequestBuilder<BackgroundSearchService>(
-                AppConstants.SERVICE_REPEAT_INTERVAL,
-                TimeUnit.MINUTES
-            )
-                .setConstraints(constraints)
-                .setBackoffCriteria(
-                    BackoffPolicy.LINEAR,
-                    AppConstants.SERVICE_REPEAT_INTERVAL,
-                    TimeUnit.MINUTES
-                )
+            val job = jobInfo.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPeriodic(AppConstants.SERVICE_REPEAT_INTERVAL)
+                .setPersisted(true)
                 .build()
 
-            WorkManager.getInstance(context)
-                .enqueue(workRequest)
-//                .enqueueUniquePeriodicWork(AppConstants.UNIQUE_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, workRequest)
+            jobScheduler.schedule(job)
         }
+
+//        fun scheduleNewWork(context: Context) {
+//
+//            val constraints =
+//                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+//
+//            val workRequest = PeriodicWorkRequestBuilder<BackgroundSearchService>(
+//                AppConstants.SERVICE_REPEAT_INTERVAL,
+//                TimeUnit.MINUTES
+//            )
+//                .setConstraints(constraints)
+//                .setBackoffCriteria(
+//                    BackoffPolicy.LINEAR,
+//                    AppConstants.SERVICE_REPEAT_INTERVAL,
+//                    TimeUnit.MINUTES
+//                )
+//                .build()
+//
+//            WorkManager.getInstance(context)
+//                .enqueue(workRequest)
+////                .enqueueUniquePeriodicWork(AppConstants.UNIQUE_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, workRequest)
+//        }
     }
 }
