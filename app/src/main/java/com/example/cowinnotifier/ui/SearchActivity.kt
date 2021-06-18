a6d5431c14cdb354910dfec4c3f3835b758a4e8e
Ujjwal <ujjwalkr.maharana@gmail.com>
2021-06-19 01:25:23 +0530

    added 1st parameter layout

---
+++ app/src/main/java/com/example/cowinnotifier/ui/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cowinnotifier.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cowinnotifier.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
